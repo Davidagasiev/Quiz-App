060dc82738512c14a5d43ccc5fa7aecaf12147fa
Davidagasiev <davidagasiev2005@gmail.com>
2022-03-08 17:24:41 +0400

    Updates

---
+++ precache-manifest.e68289e9631c1d80c93358ce8c06bccc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53a3aee9d367321ee999a862440ad0a8",
+    "url": "/Quiz-App/index.html"
+  },
+  {
+    "revision": "802a6f69219001df010b",
+    "url": "/Quiz-App/static/css/main.91250144.chunk.css"
+  },
+  {
+    "revision": "d3f244408719fc8a02cf",
+    "url": "/Quiz-App/static/js/2.ae83015b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Quiz-App/static/js/2.ae83015b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "802a6f69219001df010b",
+    "url": "/Quiz-App/static/js/main.920fe2cc.chunk.js"
+  },
+  {
+    "revision": "dc6ef8ecda0e37be7997",
+    "url": "/Quiz-App/static/js/runtime-main.4eede497.js"
+  }
+]);
